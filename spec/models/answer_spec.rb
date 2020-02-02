ef79f0a15774e6474c30211716ab360aa062e867
Bulat Khamitov <bulat.khamitov@gmail.com>
2020-02-02 18:33:01 +0300

    Enhance specs for Answer and Question models

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it { should belong_to(:question) }
+  describe 'associations' do
+    it { should belong_to(:question) }
+  end
 
-  it { should validate_presence_of :body }
+  describe 'validations' do
+    it { should validate_presence_of :body }
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it { should have_many(:answers) }
+  describe 'associations' do
+    it { should have_many(:answers) }
+  end
 
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :body }
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :body }
+  end
 end
