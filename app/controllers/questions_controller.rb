b521aab62285fdbae9be192d07c2aa05f84e9418
Bulat Khamitov <bulat.khamitov@gmail.com>
2020-02-17 14:26:39 +0300

    Patch QuestionsController

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,16 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_question, only: [:show, :edit, :update, :destroy]
+
   def index
     @questions = Question.all
   end
 
   def show; end
 
-  def new; end
+  def new
+    @question = Question.new
+  end
 
   def edit; end
 
@@ -13,14 +18,14 @@ class QuestionsController < ApplicationController
     @question = Question.new(question_params)
 
     if @question.save
-      redirect_to @question
+      redirect_to @question, notice: 'Your question successfully created.'
     else
       render :new
     end
   end
 
   def update
-    if question.update(question_params)
+    if @question.update(question_params)
       redirect_to @question
     else
       render :edit
@@ -28,17 +33,15 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    question.destroy
+    @question.destroy
     redirect_to questions_path
   end
 
   private
 
-  def question
-    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
+  def find_question
+    @question = Question.find(params[:id])
   end
-
-  helper_method :question
 
   def question_params
     params.require(:question).permit(:title, :body)
